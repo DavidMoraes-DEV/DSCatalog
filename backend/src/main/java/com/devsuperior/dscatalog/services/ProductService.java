be213b50c98d591cdec2669be56c5620bc18cab7
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-07-10 17:19:05 -0300

    Criacao dos metodos insert, update para produtos, FINALIZANDO completamente o CRUD desse projeto

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -23,6 +26,9 @@ public class ProductService {
 
 	@Autowired 
 	private ProductRepository repository;
+	
+	@Autowired 
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -43,11 +49,12 @@ public class ProductService {
 		return new ProductDTO(entity, entity.getCategories());
 	}
 
+	//A diferença em inserir e atualizar um produto em relação com a categoria é que vai mudar os dados que serão utilizados, pois na categoria era apenas o nome e os produtos tem mais dados diferentes para incluir
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity); //Utilizamos então um método auxiliar para realizar a cópia dos dados do produto para não utilizar vários SETs nesse método
 		
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
@@ -57,7 +64,8 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getOne(id); 
-			//entity.setName(dto.getName()); 
+			copyDtoToEntity(dto, entity);
+			
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 			
@@ -74,6 +82,23 @@ public class ProductService {
 			throw new ResourceNotFoundException("Id not found " + id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataBaseException("Integrity Violation");
+		}
+	}
+	
+	//Associa categorias que é uma entidade dentro de outra entidade que é PRODUCT para que sejá salva(inserida) ou atualizadas
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		//Não se copia o Id, porque não se troca ele na hora de ATUALIZAR e também não informa ele na hora de INSERIR, por isso o id não entra no método de copiar os dados
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear(); //Limpa o conjunto de categorias que podem estar na entidade
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category); //Coleção do tipo Product pronta para ser inserida ou atualizada
 		}
 	}
 }
