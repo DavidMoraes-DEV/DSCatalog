b52ebb1b00b62477628151a745fb45db30100216
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-07-14 15:00:14 -0300

    Testando outro cenario do delete quando o ID NAO existe

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
 
@@ -15,7 +16,7 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
-	//Testa se realmente esta deletando no ProductService pelo id
+	//Testa se realmente esta deletando no ProductService pelo id QUANDO o id existe
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
 		
@@ -25,5 +26,18 @@ public class ProductRepositoryTests {
 		Optional<Product> result = repository.findById(exintingId); //busca pelo id do Product que acabou de ser deletado para testar se realmente foi deletado
 		
 		Assertions.assertFalse(result.isPresent()); //O assertFalse() espera um result = FALSE e o .isPresent() testa se existe um objeto dentro do Optional<Product>, que nesse caso não é para existir pois foi deletado antes
+	}
+	
+	//Testa se realmente lança a exceção quando a deleção for chamada mas o ID NÃO EXISTE
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			
+			long nonExintingId = 100L;
+			
+			repository.deleteById(nonExintingId);
+		});
+		
 	}
 }
