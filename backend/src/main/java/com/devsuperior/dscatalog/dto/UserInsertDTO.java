590fb7ea7d1ac43016168392fbe30c01cb4712f2
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-07-22 10:33:48 -0300

    Implementando um ConstraintValidator customizado para NAO PERMITIR dois usuarios com o mesmo EMAIL

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dscatalog.dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
 //Classe que herda de UserDTO, ou seja tem todas as funcionalidades do UserDTO porém com a adição de colocar possuir também o atributo password
+@UserInsertValid //Essa Annotation que vai processar em segundo plano a verificação se o email inserido já existe no banco, aproveitando todo o ciclo de vida da validação que estamos utilizando
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 	
--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -8,4 +8,5 @@ import com.devsuperior.dscatalog.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	User findByEmail(String email); //Busca no banco um usuário por email
 }
