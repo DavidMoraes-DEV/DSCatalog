7faebde9388357ab6d642b369da201b0e39ae1ad
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-07-26 17:36:45 -0300

    Implementando uma configuração para liberar acesso ao H2 quando o PROFILE for TEST

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,15 +17,24 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 	
 	//Constantes por questão de organização definindo as rotas
-	private static final String[] PUBLIC = {"/oauth/token"}; //Rota para os endPointers PUBLICOS(Acesso para todos sem a necessidade de já ter feito login
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"}; //Rota para os endPointers PUBLICOS(Acesso para todos sem a necessidade de já ter feito login
 	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"}; //Rotas para ser liberadas apenas para usuários que tem os perfis de operador e admin
 	private static final String[] ADMIN = {"/users/**"}; //Rotas específicas para usuários com o perfil admins
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//Evita o erro do H2 ao definir autorizações para as rotas e exigências de login
+		//Se lê: SE nos profiles ativos contém o profile TEST irá executar o código para liberar o H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) { //Verifica se estiver rodando um profile TEST precisaremos liberar o H2
+			http.headers().frameOptions().disable(); //Se o teste do if for verdadeiro irá liberar os frames do H2 para conseguirmos acessa-lo pelo localhost:8080
+		}
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll() //antMatchers() é o método que define as autorizações passando a rota deseja, .permitAll() permite todos os acessos não exigindo a realização de login
