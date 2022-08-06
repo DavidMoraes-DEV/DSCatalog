dead52bc7d34b8567b3d0915df416650bb077115
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-06 13:17:01 -0300

    Maneira de EVITAR o retorno de uma lista VAZIA quando nao especificar a categoria na consulta

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -22,8 +22,17 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
 				- Se não colocar o apelido para as categorias irá dar um ERRO DE SINTAX de SQL
 					- Coloca o Product com sua categorias
 					- Forçando o JPA a fazer uma consulta para fazer o INNER JOIN das duas tabelas
+			
+			- Para evitar o erro de buscar uma lista VAZIA quando a categoria for 0 ou NULL(Quando não ESPECIFICAR o ID da categoria na consulta)
+				- Utiliza-se o IS NULL OR
+					- Para que se der verdadeiro nem irá executar o restante pois já foi definido que a categoria é nula 
+						- Não buscará os produtos com as categorias e retornará apenas os produtos com as categorias VAZIAS
+						- Ficando então a consulta: WHERE ( :category IS NULL OR :category IN catg)
+							- Mas nesse caso retornará uma LINHAA A MAIS pois o JOIN volta as linhas repetidas
+							- Para EVITAR ESSAS REPETIÇÕES utilizamos a claúsula DISTINCT para não haver repetições de produtos
+						
 	*/
-	@Query("SELECT obj FROM Product obj INNER JOIN obj.categories catg WHERE "
-			+ ":category IN catg")
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories catg WHERE "
+			+ "( :category IS NULL OR :category IN catg)")
 	Page<Product> findAllProductCategory(Category category, Pageable pageable);
 }
