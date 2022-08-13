18a5fc4536a1cb44c3bb25f3b23d1f9714947529
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-13 14:37:32 -0300

    Instalando a extencao Prettier Code Formatter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import  './assets/styles/custom.scss';
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hello DSCatalog</h1>
-  );
+  return <h1>Hello DSCatalog</h1>;
 }
 
 export default App;
