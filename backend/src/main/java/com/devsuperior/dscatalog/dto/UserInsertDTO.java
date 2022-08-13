390edf91c109458bf144226a862189fd1322f422
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-13 12:46:37 -0300

    Inicio do desenvolvimento do FrontEnd

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -10,11 +10,9 @@ public class UserInsertDTO extends UserDTO {
 	private String password;
 	
 	public UserInsertDTO () {
-		super();
 	}
 
 	public UserInsertDTO(String password) {
-		super();
 		this.password = password;
 	}
 
