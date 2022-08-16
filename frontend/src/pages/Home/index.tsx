ac2c64cbdca6a4ddb213321941210967bbf531ac
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-16 16:01:53 -0300

    Finalizando a estilizacao da menor tela conhecida como X-Small

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from "assets/images/main-image.svg";
+import ButtonIcon from "components/ButtonIcon";
 import Navbar from "components/Navbar";
 
 import './styles.css'
@@ -11,6 +12,8 @@ const Home = () => {
             <div className="home-card">
                 <div className="home-content-container">
                     <h1>Conheça o melhor catálogo de produtos</h1>
+                    <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
+                    <ButtonIcon />
                 </div>
                 <div className="home-image-container">
                     <MainImage />
