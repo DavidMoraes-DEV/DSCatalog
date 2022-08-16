cb6c3a27458f5d56d8f0277a0cc7fa35d1b7dda1
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-16 14:35:31 -0300

    Implementando o menu hamburguer da Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,13 +1,27 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => {
     return (
-      <nav className="navbar navbar-expand-md bg-primary main-nav">
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
         <div className='container-fluid'>
             <a href="link"className="nav-logo-text">
                 <h4>DS Catalog</h4>
             </a>
-            <div className='collapse navbar-collapse'>
+
+            <button
+                className='navbar-toggler'
+                type='button'
+                data-bs-toggle='collapse'
+                data-bs-target='#dscatalog-navbar'
+                aria-controls='dscatalog-navbar'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+            >
+                <span className='navbar-toggler-icon'></span>
+            </button>
+
+            <div className='collapse navbar-collapse' id="dscatalog-navbar">
                 <ul className='navbar-nav offset-md-2 main-menu'>
                     <li>
                         <a href="link" className='active'>HOME</a>
