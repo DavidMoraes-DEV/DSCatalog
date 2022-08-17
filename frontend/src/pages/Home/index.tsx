0f19aba86b94e03a45e2e055f0c500263c2d9d18
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-17 19:08:40 -0300

    Finalizando a responsividade da tela HOME com tamanho maior que 1200px

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -11,8 +11,10 @@ const Home = () => {
        <div className="home-container">
             <div className="home-card">
                 <div className="home-content-container">
-                    <h1>Conheça o melhor catálogo de produtos</h1>
-                    <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
+                    <div>
+                        <h1>Conheça o melhor catálogo de produtos</h1>
+                        <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>    
+                    </div>
                     <ButtonIcon />
                 </div>
                 <div className="home-image-container">
