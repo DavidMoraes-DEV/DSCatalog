b5a495e2156c923276fa6a0a94340e4779b76143
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-18 17:37:11 -0300

    Customizando o ButtonIcon para ficar igual ao do Projeto do Figma

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -1,11 +1,17 @@
-import './styles.css'
+import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
 const ButtonIcon = () => {
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIE AGORA A SUA BUSCA</h6>
-        </button>
-    )
-}
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>INICIE AGORA A SUA BUSCA</h6>
+      </button>
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
+  );
+};
 
 export default ButtonIcon;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,29 +1,33 @@
-import { ReactComponent as MainImage } from "assets/images/main-image.svg";
-import ButtonIcon from "components/ButtonIcon";
-import { Link } from "react-router-dom";
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
+import { Link } from 'react-router-dom';
 
-import './styles.css'
+import './styles.css';
 
 const Home = () => {
-    return (
-       <div className="home-container">
-            <div className="base-card home-card">
-                <div className="home-content-container">
-                    <div>
-                        <h1>Conheça o melhor catálogo de produtos</h1>
-                        <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>    
-                    </div>
-                    <Link to='/products'>
-                        <ButtonIcon />
-                    </Link>
-                </div>
-                <div className="home-image-container">
-                    <MainImage />
-                </div>
-            </div>
-       </div>
-    ); 
-  }
-  
-  export default Home;
-  
+  return (
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div>
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>
+              Ajudaremos você a encontrar os melhores produtos disponíveis no
+              mercado
+            </p>
+          </div>
+          <div>
+            <Link to="/products">
+              <ButtonIcon />
+            </Link>
+          </div>
+        </div>
+        <div className="home-image-container">
+          <MainImage />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
