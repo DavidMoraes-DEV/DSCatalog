3deebd41d3c6801fd33d8250c3fbfeabb8122da0
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-20 14:30:26 -0300

    Formatando os numeros que sao precos com o Intl.NumberFormat

--- frontend/src/components/ProductPrice/index.tsx
+++ frontend/src/components/ProductPrice/index.tsx
@@ -1,4 +1,5 @@
 
+import { formatPrice } from 'util/formatters';
 import './styles.css';
 
 type Props = {
@@ -9,7 +10,7 @@ const ProductPrice = ( { price } : Props) => {
     return (
         <div className='product-price-container'>
             <span>R$</span>
-            <h3>{price}</h3>
+            <h3>{formatPrice(price)}</h3>
         </div>
     );
 }
