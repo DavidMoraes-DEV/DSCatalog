b184c3c17146a1164fdd956adb5ebd018d90205b
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-20 15:30:32 -0300

    Implementando a Pagina Detalhada de Produtos e definindo sua Rota

--- frontend/src/RoutesDom.tsx
+++ frontend/src/RoutesDom.tsx
@@ -2,6 +2,7 @@ import Navbar from "components/Navbar";
 import Admin from "pages/Admin";
 import Catalog from "pages/Catalog";
 import Home from "pages/Home";
+import ProductDetails from "pages/ProductDetails";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -12,8 +13,9 @@ const RoutesDom = () => (
         <Routes>
             <>
                 <Route path='/' element={<Home />} />
-                <Route path='/products' element={<Catalog />} />
                 <Route path='/admin' element={<Admin />} />
+                <Route path='/products' element={<Catalog />} />
+                <Route path='/products/:productId' element={<ProductDetails />} />
             </>
         </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -0,0 +1,9 @@
+
+
+const ProductDetails = () => {
+    return (
+        <h1>Product Details</h1>
+    );
+}
+
+export default ProductDetails;
