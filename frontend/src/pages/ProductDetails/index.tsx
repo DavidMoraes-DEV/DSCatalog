a0c720e4762d0053e68975eb6a98160345e1bf12
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-20 15:53:36 -0300

    Implementando a estrutura basica da pagina ProductDetails

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,8 +1,33 @@
-
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
 
 const ProductDetails = () => {
     return (
-        <h1>Product Details</h1>
+        <div className="products-details-container">
+            <div className="product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do Produto</h1>
+                            <ProductPrice price={2345.67}/>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do Produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, neque!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
