eb89b8d6dc8c7e053ad3595bc37b2a2fea0537bb
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-20 14:22:19 -0300

    Incluindo Props nos argumentos dos componentes ProductPrice e ProductCard

--- frontend/src/components/ProductCard/index.tsx
+++ frontend/src/components/ProductCard/index.tsx
@@ -1,18 +1,22 @@
 import './styles.css';
 
-import ProductImg from 'assets/images/product.png';
 import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/products';
 
-const ProductCard = () => {
+type Props = {
+    product: Product;
+}
+
+const ProductCard = ( { product } : Props) => {
 
     return (
         <div className='base-card product-card'>
             <div className='card-top-container'>
-                <img src={ProductImg} alt="Nome do produto" />
+                <img src={product.imgUrl} alt={product.name} />
             </div>
             <div className='card-bottom-container'>
-                <h6>Nome do Produto</h6>
-                <ProductPrice />
+                <h6>{product.name}</h6>
+                <ProductPrice price={product.price}/>
             </div>
         </div>
     );
--- frontend/src/components/ProductPrice/index.tsx
+++ frontend/src/components/ProductPrice/index.tsx
@@ -1,11 +1,15 @@
 
 import './styles.css';
 
-const ProductPrice = () => {
+type Props = {
+    price: number;
+}
+
+const ProductPrice = ( { price } : Props) => {
     return (
         <div className='product-price-container'>
             <span>R$</span>
-            <h3>2345.67</h3>
+            <h3>{price}</h3>
         </div>
     );
 }
