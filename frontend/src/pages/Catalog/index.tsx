d2e0136a8c55d47c07f1b4271c92b5f560f466a5
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-21 18:39:54 -0300

    Rederizando dinamicamente os cards do Catalog

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -29,18 +29,6 @@ const Catalog = () => {
     });
   }, []);
 
-  const product = {
-    id: 1,
-    name: 'The Lord of the Rings',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 90.5,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
-    date: '2020-07-13T20:50:07.123450Z',
-    categories: [],
-  };
-
   return (
     <div className="container my-4 catalog-container">
       <div className="row catalog-title-container">
@@ -48,68 +36,17 @@ const Catalog = () => {
       </div>
 
       <div className="row">
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-xl-3">
-          <Link to={'/products/1'}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
+        {page?.content.map(product => {
+          return (
+            <div className="col-sm-6 col-md-4 col-xl-3" key={product.id}>
+              <Link to={'/products/1'}>
+                <ProductCard product={product} />
+              </Link>
+            </div>
+          );
+        })}
       </div>
-
+      
       <div className="row">
         <Pagination />
       </div>
