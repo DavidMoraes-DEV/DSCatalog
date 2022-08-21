98fb028ef51a35c9fd88f14c33ab7c09e64ceb0a
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-20 22:32:53 -0300

    Layout responsivo basico da pagina Admin e sua Navbar exclusiva

--- frontend/src/pages/Admin/Navbar/index.tsx
+++ frontend/src/pages/Admin/Navbar/index.tsx
@@ -1,21 +1,27 @@
-
+import './styles.css';
 
 const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <p>Produtos</p>
-                </li>
-                <li>
-                    <p>Categorias</p>
-                </li>
-                <li>
-                    <p>Usuários</p>
-                </li>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className="admin-nav-container">
+      <ul>
+        <li>
+          <a href="link" className='admin-nav-item active'>
+            <p>Produtos</p>
+          </a>
+        </li>
+        <li>
+          <a href="link" className='admin-nav-item'>
+            <p>Categorias</p>
+          </a>
+        </li>
+        <li>
+          <a href="link" className='admin-nav-item'>
+            <p>Usuários</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import './styles.css';
 
 const Admin = () => {
 
