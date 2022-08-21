7fd8c0238eabf1144b06d9dd58e4e10d35737ef0
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-21 18:28:05 -0300

    Criando a requisicao paginada de produtos para exibir no Catalog

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,35 +1,49 @@
+import axios from 'axios';
 import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/products';
+import { SpringPage } from 'types/spring';
+import { AxiosParams } from 'types/vendor/axios';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
 const Catalog = () => {
-  const product: Product = {
-    id: 2,
-    name: 'Smart TV',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
+      },
+    };
+
+    axios(params).then((response) => {
+      setPage(response.data);
+      console.log(page);
+    });
+  }, []);
+
+  const product = {
+    id: 1,
+    name: 'The Lord of the Rings',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 2190.0,
+    price: 90.5,
     imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Livros',
-      },
-      {
-        id: 3,
-        name: 'Computadores',
-      },
-    ],
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
+    date: '2020-07-13T20:50:07.123450Z',
+    categories: [],
   };
 
   return (
     <div className="container my-4 catalog-container">
-      <div className='row catalog-title-container'>
+      <div className="row catalog-title-container">
         <h1>Catálogo de produtos</h1>
       </div>
 
@@ -96,7 +110,7 @@ const Catalog = () => {
         </div>
       </div>
 
-      <div className='row'>
+      <div className="row">
         <Pagination />
       </div>
     </div>
