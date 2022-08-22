c07793ee8362d826377b015ef05012db496bb423
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-22 15:13:11 -0300

    Estilizando o Loader da pagina Catalog

--- frontend/src/pages/Catalog/CardLoader/index.tsx
+++ frontend/src/pages/Catalog/CardLoader/index.tsx
@@ -1,16 +1,18 @@
-import ContentLoader from 'react-content-loader'
+import ContentLoader from 'react-content-loader';
 
 const CardLoader = () => (
+  <div className='card-loader-container'>
     <ContentLoader
-        speed={2}
-        width={400}
-        height={460}
-        viewBox = '0 0 400 400'
-        backgroundColor='#f3f3f3'
-        foregroundColor='#ecebeb'
+      speed={2}
+      width={300}
+      height={460}
+      viewBox="0 0 300 400"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
     >
-        <rect x='7' y='18' rx='2' ry='2' width='300' height='300' />
+      <rect x="0" y="0" rx="2" ry="2" width="300" height="300" />
     </ContentLoader>
-)
+  </div>
+);
 
 export default CardLoader;
