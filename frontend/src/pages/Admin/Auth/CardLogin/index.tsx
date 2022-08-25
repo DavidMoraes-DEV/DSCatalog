ae1a30233fcb35619c314023c0a64a8ca89b4be4
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-25 17:25:43 -0300

    Implementando o HTML e Estilizando o CardLogin

--- frontend/src/pages/Admin/Auth/CardLogin/index.tsx
+++ frontend/src/pages/Admin/Auth/CardLogin/index.tsx
@@ -1,11 +1,44 @@
+import { Link } from 'react-router-dom';
+import ButtonIcon from 'components/ButtonIcon';
+ 
 import './styles.css';
-
+ 
 const CardLogin = () => {
-    return (
-        <div className='card-login-container'>
-        <h1>Card Login</h1>
-        </div>      
-    );
-}
+  return (
+    <div className="base-card login-card-container">
+      <h1>LOGIN</h1>
+      <form>
+        <div className="mb-4">
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="password"
+            className="form-control base-input "
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default CardLogin;
