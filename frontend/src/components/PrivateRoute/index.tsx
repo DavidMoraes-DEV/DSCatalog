b0371d247e6b39fa54a4798c7b16560f66688a3b
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-09-07 14:22:20 -0300

    Componente PrivateRoute para rotas autorizadas

---
+++ frontend/src/components/PrivateRoute/index.tsx
@@ -0,0 +1,18 @@
+import { Redirect, Route } from 'react-router-dom';
+import { isAuthenticated } from 'util/requests';
+ 
+type Props = {
+  children: React.ReactNode;
+  path: string;
+};
+ 
+const PrivateRoute = ({ children, path }: Props) => {
+  return (
+    <Route
+      path = {path}
+      
+    />
+  );
+};
+ 
+export default PrivateRoute;
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,4 +1,5 @@
-import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from 'components/PrivateRoute';
+import { Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import './styles.css';
 import Users from './User';
@@ -9,15 +10,15 @@ const Admin = () => {
       <Navbar />
       <div className="admin-content-container">
         <Switch>
-          <Route path="/admin/products">
+          <PrivateRoute path="/admin/products">
             <h1>Product CRUD</h1>
-          </Route>
-          <Route path="/admin/categories">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/categories">
             <h1>Category CRUD</h1>
-          </Route>
-          <Route path="/admin/users">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/users">
             <Users />
-          </Route>
+          </PrivateRoute>
         </Switch>
       </div>
     </div>
