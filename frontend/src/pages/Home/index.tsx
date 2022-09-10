5100d1f4b2caaac13e5a10e1f33d95ccb2aed2d5
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-09-10 18:20:42 -0300

    Implementando a funcao hasAnyRoles

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { getTokenData, isAuthenticated } from 'util/requests';
+import { hasAnyRoles } from 'util/requests';
 
 import './styles.css';
 
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div className="home-container">
 
-      <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
+      <h1>Resultado = {hasAnyRoles(["ROLE_ADMIN"]) ? 'Sim' : 'Não'}</h1>
 
       <div className="base-card home-card">
         <div className="home-content-container">
--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -116,3 +116,20 @@ export const isAuthenticated = () : boolean => {
   const tokenData = getTokenData();
   return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+  if (tokenData !== undefined) {
+    for (var i = 0; i< roles.length; i++) {
+      if(tokenData.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
