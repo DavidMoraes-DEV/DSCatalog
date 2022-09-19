fe38c54d76f6e874f9813ca530f5e0bb84525c6a
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-09-19 12:34:00 -0300

    Implementando e estilizando o formulario de produtos do ProductCRUD

--- frontend/src/pages/Admin/CRUDS/ProductsCRUD/Form/index.tsx
+++ frontend/src/pages/Admin/CRUDS/ProductsCRUD/Form/index.tsx
@@ -1,5 +1,45 @@
+import './styles.css';
+
 const Form = () => {
-  return <h1>Tela Form</h1>;
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+        <form action="">
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-botton-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-botton-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  name=""
+                  rows={10}
+                  className="form-control base-input h-auto"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
--- frontend/src/pages/Admin/CRUDS/ProductsCRUD/List/index.tsx
+++ frontend/src/pages/Admin/CRUDS/ProductsCRUD/List/index.tsx
@@ -29,7 +29,7 @@ const List = () => {
   };
 
   return (
-    <>
+    <div className='product-crud-container'>
       <div className="product-crud-bar-container">
         <Link to="/admin/products/create">
           <button className="btn btn-primary text-white btn-crud-add">
@@ -52,7 +52,7 @@ const List = () => {
         <ProductCrudCard product={product} />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
