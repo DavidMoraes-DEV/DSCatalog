914233ce6de300fbb4bf769b3082f93f0687d893
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-09-19 15:28:34 -0300

    Deletando um produto com o padrao de projetos OBSERVER

--- frontend/src/pages/Admin/CRUDS/ProductsCRUD/List/index.tsx
+++ frontend/src/pages/Admin/CRUDS/ProductsCRUD/List/index.tsx
@@ -9,28 +9,28 @@ import { requestBackend } from 'util/requests';
 
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
-  
+
   useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
         page: 0,
         size: 50,
-      }
+      },
     };
 
-    
-    requestBackend(config)
-      .then((response) => {
-        setPage(response.data);
-        console.log(page);
-      })
-  }, []);
-
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  };
 
   return (
-    <div className='product-crud-container'>
+    <div className="product-crud-container">
       <div className="product-crud-bar-container">
         <Link to="/admin/products/create">
           <button className="btn btn-primary text-white btn-crud-add">
@@ -39,11 +39,11 @@ const List = () => {
         </Link>
         <div className="base-card product-filter-container">Search bar</div>
       </div>
-      <div className='row'>
-        { page?.content.map(product => (
-        <div className='col-sm-6 col-md-12' key={product.id}>
-        <ProductCrudCard product={product} />
-        </div>
+      <div className="row">
+        {page?.content.map((product) => (
+          <div className="col-sm-6 col-md-12" key={product.id}>
+            <ProductCrudCard product={product} onDelete={() => getProducts()} />
+          </div>
         ))}
       </div>
     </div>
--- frontend/src/pages/Admin/CRUDS/ProductsCRUD/ProductCrudCard/index.tsx
+++ frontend/src/pages/Admin/CRUDS/ProductsCRUD/ProductCrudCard/index.tsx
@@ -8,12 +8,13 @@ import { requestBackend } from 'util/requests';
 
 type Props = {
   product: Product;
+  onDelete: Function;
 };
 
-const ProductCrudCard = ({ product }: Props) => {
+const ProductCrudCard = ({ product, onDelete }: Props) => {
   const handleDelete = (productId: number) => {
 
-    if (!window.confirm('Tem certeza que seja deletar?')) {
+    if (!window.confirm('Tem certeza que seja Excluir esse produto?')) {
       return;
     }
 
@@ -24,7 +25,7 @@ const ProductCrudCard = ({ product }: Props) => {
     };
 
     requestBackend(config).then(() => {
-      console.log('DELETADO ID ' + productId);
+      onDelete();
     });
   };
 
