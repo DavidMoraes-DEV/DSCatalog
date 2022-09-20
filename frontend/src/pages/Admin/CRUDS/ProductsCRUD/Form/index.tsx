e606974b32591638ec7441b1be992cac5478aa3c
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-09-20 09:54:10 -0300

    Removendo hardcode dos campos imgUrl e categories

--- frontend/src/pages/Admin/CRUDS/ProductsCRUD/Form/index.tsx
+++ frontend/src/pages/Admin/CRUDS/ProductsCRUD/Form/index.tsx
@@ -47,18 +47,11 @@ const Form = () => {
   }, [isEditing, productId, setValue]);
 
   const onSubmit = (formData: Product) => {
-    const data = {
-      ...formData,
-      imgUrl: isEditing
-        ? formData.imgUrl
-        : 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
-      categories: isEditing ? formData.categories : [{ id: 1, name: 'teste' }],
-    };
 
     const config: AxiosRequestConfig = {
       method: isEditing ? 'PUT' : 'POST',
       url: isEditing ? `/products/${productId}` : '/products',
-      data,
+      data: formData,
       withCredentials: true,
     };
 
@@ -137,6 +130,27 @@ const Form = () => {
                 </div>
               </div>
             </div>
+
+            <div className="margin-botton-30">
+                <input
+                  {...register('imgUrl', {
+                    required: '* Campo Obrigatório',
+                    pattern: {
+                      value: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
+                      message: 'Deve ser uma URL válida'
+                    }
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="URL da imagem do Produto"
+                  name="imgUrl"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.imgUrl?.message}
+                </div>
+              </div>
 
             <div className="col-lg-6">
               <div>
