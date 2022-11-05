cb4f4af8394605ace1954617e5a0533b5bb05cba
David Moraes de Oliveira <DavidMoraes-DEV@outlook.com>
2022-11-05 15:11:34 -0300

    ButtonIcon first test

---
+++ frontend/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import ButtonIcon from "..";
+
+
+test('ButtonIcon should render button with given text', () => {
+
+    const text = "Fazer login";
+    
+    render(
+        <ButtonIcon text={text} />
+    );
+
+    screen.debug();
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+});
+
+/*
+Se estiver usando React <= 17 utilizar o comando:
+yarn add @testing-library/react@release-12.x para não dar erro de incompatibilidade ao rodar os tests
+React 18 utilizar yarn add @testing-library/react
+*/
