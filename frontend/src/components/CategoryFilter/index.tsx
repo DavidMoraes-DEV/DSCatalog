3ed82f78092b7f45a4f097258455df2aecff2286
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-12-09 17:43:17 -0300

    Integrated CategoryFilter

--- frontend/src/components/CategoryFilter/index.tsx
+++ frontend/src/components/CategoryFilter/index.tsx
@@ -1,24 +1,75 @@
 import { ReactComponent as SearchIcon } from 'assets/images/search-icon.svg';
 import './styles.css';
 import Select from 'react-select';
+import { Controller, useForm } from 'react-hook-form';
+import { Category } from 'types/category';
+import { useEffect, useState } from 'react';
+import { requestBackend } from 'util/requests';
+import { FilterOptions } from 'types/filterOptions';
 
-const CategoryFilter = () => {
+export type CategoryFilterData = {
+  textInput?: string;
+  category: Category | null;
+  option: FilterOptions | null;
+};
 
-  const options = [
-    {value: 'Antigos', label: 'Mais Antigos'},
-    {value: 'Recentes', label: 'Mais Recentes'},
-    {value: 'Ordem Alfabética', label: 'Ordem Alfabética'}
-  ]
+type Props = {
+  onSubmitFilter: (data: CategoryFilterData) => void;
+};
+
+const CategoryFilter = ({ onSubmitFilter }: Props) => {
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
+
+  const { register, handleSubmit, setValue, getValues, control } =
+    useForm<CategoryFilterData>();
+
+  useEffect(() => {
+    requestBackend({ url: '/categories' }).then((response) => {
+      setSelectCategories(response.data.content);
+    });
+  }, []);
+
+  const onSubmit = (formData: CategoryFilterData) => {
+    const selectedCategory = selectCategories.find(
+      (catg) => catg.name === formData.textInput
+    );
+    if (selectedCategory !== undefined) {
+      formData.category = selectedCategory;
+    } else {
+      formData.category = null;
+    }
+
+    onSubmitFilter(formData);
+  };
+
+  const handleFormClear = () => {
+    setValue('textInput', '');
+    setValue('category', null);
+    setValue('option', null);
+  };
+
+  const handleChangeOption = (value: FilterOptions) => {
+    setValue('option', value);
+
+    const obj: CategoryFilterData = {
+      textInput: getValues('textInput'),
+      category: getValues('category'),
+      option: getValues('option'),
+    };
+
+    onSubmitFilter(obj);
+  };
 
   return (
     <div className="base-card category-filter-container">
-      <form onSubmit={() => {}} className="category-filter-form">
+      <form onSubmit={handleSubmit(onSubmit)} className="category-filter-form">
         <div className="category-filter-name-container">
           <input
+            {...register('textInput')}
             type="text"
             className="form-control"
             placeholder="Pesquisar Categoria"
-            name="name"
+            name="textInput"
           />
           <button className="category-filter-search-icon">
             <SearchIcon />
@@ -26,16 +77,31 @@ const CategoryFilter = () => {
         </div>
         <div className="category-filter-botton-container">
           <div className="category-filter-category-container">
+            <Controller
+              name="option"
+              control={control}
+              render={({ field }) => (
                 <Select
-                  options={options}
+                  {...field}
+                  options={[
+                    { id: 1, name: 'Mais Antigos' },
+                    { id: 2, name: 'Mais Recentes' },
+                    { id: 3, name: 'Ordem Alfabética' },
+                  ]}
                   isClearable
                   classNamePrefix="category-filter-select"
                   placeholder="Filtrar por"
-                  onChange={() => {}}
+                  onChange={(value) =>
+                    handleChangeOption(value as FilterOptions)
+                  }
+                  getOptionLabel={(option: FilterOptions) => option.name}
+                  getOptionValue={(option: FilterOptions) => String(option.id)}
                 />
+              )}
+            />
           </div>
           <button
-            onClick={() => {}}
+            onClick={handleFormClear}
             className="btn btn-outline-secondary btn-category-filter-clear"
           >
             LIMPAR <span className="btn-category-filter-word">FILTRO</span>
---
+++ frontend/src/types/filterOptions.ts
@@ -0,0 +1,4 @@
+export type FilterOptions = {
+    id: number;
+    name: string;
+}
