9d83ba7f58658543daac1c9537d91a06a75f421b
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-12-09 17:48:59 -0300

    Integrated CategoryList, CategoryCrudCard

--- frontend/src/pages/Admin/CRUDS/CategoriesCRUD/CategoryCrudCard/index.tsx
+++ frontend/src/pages/Admin/CRUDS/CategoriesCRUD/CategoryCrudCard/index.tsx
@@ -1,18 +1,42 @@
 import './styles.css';
+import { Category } from 'types/category';
 import { Link } from 'react-router-dom';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
-const CategoryCrudCard = () => {
+type Props = {
+  category: Category;
+  onDelete: Function;
+};
+
+const CategoryCrudCard = ({ category, onDelete }: Props) => {
+  const handleDelete = (categoryId: number) => {
+    if (!window.confirm('Tem certeza que seja Excluir esse produto?')) {
+      return;
+    }
+
+    const config: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: `/categories/${categoryId}`,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      onDelete();
+    });
+  };
+
   return (
     <div className="base-card category-crud-card">
-      <h6>Livros</h6>
+      <h6>{category.name}</h6>
       <div className="category-crud-card-buttons-container">
-        <Link to={'/admin/categories/1'}>
+        <Link to={`/admin/categories/${category.id}`}>
           <button className="btn btn-outline-secondary category-crud-card-button">
             EDITAR
           </button>
         </Link>
         <button
-          onClick={() => {}}
+          onClick={() => handleDelete(category.id)}
           className="btn btn-outline-danger category-crud-card-button"
         >
           EXCLUIR
