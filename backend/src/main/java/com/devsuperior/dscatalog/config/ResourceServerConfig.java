286ca465016b99a5e7a04eaa78fc83ce296bf42e
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-12-18 16:05:07 -0300

    Update ResourceServerConfig

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -31,7 +31,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
-	private static final String[] ADMIN = {"/users/**"};
+	private static final String ADMIN = "/users/**";
+	private static final String NEW_USER = "/users";
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
@@ -42,7 +43,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() 
+		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+		.antMatchers(HttpMethod.POST, NEW_USER).permitAll()
 		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
 		.antMatchers(ADMIN).hasRole("ADMIN")
 		.anyRequest().authenticated();
