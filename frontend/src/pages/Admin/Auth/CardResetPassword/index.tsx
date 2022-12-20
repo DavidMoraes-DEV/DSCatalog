d34a800201661bb3e1757ad197fe2ba2a7066b91
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-12-20 16:09:58 -0300

    Integrated CardResetPassword

--- frontend/src/pages/Admin/Auth/CardResetPassword/index.tsx
+++ frontend/src/pages/Admin/Auth/CardResetPassword/index.tsx
@@ -1,28 +1,115 @@
 import './styles.css';
 import { ReactComponent as RevealPass } from '../../../../assets/images/reveal-password-icon.svg';
+import { ReactComponent as HiddenPass } from '../../../../assets/images/hidden-password-icon.svg';
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { requestBackend } from 'util/requests';
+import { toast } from 'react-toastify';
+import { getValue } from '@testing-library/user-event/dist/utils';
+import {
+  getPasswordResetTokenLocalStorage,
+  removePasswordResetTokenLocalStorage,
+} from 'util/storage';
+import { useState } from 'react';
 
-const CardResetPassword = () => {
-  
+type ResetPassword = {
+  password: string;
+};
+
+type Props = {
+  passwordResetToken: string;
+};
+
+const CardResetPassword = ({ passwordResetToken }: Props) => {
+  const history = useHistory();
+  const [inputTypePass, setInputTypePass] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    clearErrors,
+  } = useForm<ResetPassword>();
+
+  const onSubmit = (formData: ResetPassword) => {
+    const data = {
+      ...formData,
+    };
+
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/users/reset-password',
+      data,
+      params: {
+        token: getPasswordResetTokenLocalStorage().token,
+      },
+    };
+
+    requestBackend(config)
+      .then(() => {
+        toast.info('Senha alterada com sucesso');
+        history.push('/admin/auth/login');
+        removePasswordResetTokenLocalStorage();
+      })
+      .catch(() => {
+        toast.error('Erro ao Alterar sua senha');
+      });
+  };
+
+  const handleCancel = () => {
+    history.push('/admin/auth/login');
+  };
+
+  const revealPassword = (event: React.InputHTMLAttributes<InputEvent>) => {
+    inputTypePass === false ? setInputTypePass(true) : setInputTypePass(false);
+  };
+
+  const confirmPassword = () => {
+    const newPassword = document.getElementById('nPassword');
+    const confPassword = document.getElementById('cPassword');
+
+    if (getValue(confPassword) !== '') {
+      !(
+        (getValue(confPassword) as String) === (getValue(newPassword) as String)
+      )
+        ? setError('password', {
+            type: 'validate',
+            message: 'As senhas não correspondem',
+          })
+        : clearErrors('password');
+    }
+  };
+
   return (
     <div className="base-card reset-password-card-container">
       <div className="reset-password-card-form-container">
         <h1>CRIE UMA NOVA SENHA</h1>
 
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="reset-password-card-input-content">
             <div className="reset-password-card-input-container">
               <input
+                {...register('password', {
+                  required: 'Campo Obrigatório!',
+                })}
                 id="nPassword"
-                type='password'
-                className='form-control reset-password-card-input'
+                type={inputTypePass ? 'text' : 'password'}
+                className={`form-control reset-password-card-input ${
+                  errors.password ? 'is-invalid' : ''
+                }`}
                 placeholder="Digite uma nova senha"
                 name="password"
               />
               <span className="reset-password-card-reveal-icon">
-                <button type="button" onClick={() => {}}>
-                  <RevealPass />
+                <button type="button" onClick={revealPassword}>
+                  {inputTypePass ? <HiddenPass /> : <RevealPass />}
                 </button>
               </span>
+            </div>
+            <div className="invalid-feedback d-block">
+              {errors.password?.message}
             </div>
           </div>
 
@@ -30,17 +117,22 @@ const CardResetPassword = () => {
             <div className="reset-password-card-input-container">
               <input
                 id="cPassword"
-                type='password'
-                className='form-control reset-password-card-input'
+                type={inputTypePass ? 'text' : 'password'}
+                className={`form-control reset-password-card-input ${
+                  errors.password ? 'is-invalid' : ''
+                }`}
                 placeholder="Confirme sua nova senha"
                 name="confPassword"
-                onChange={() => {}}
+                onChange={confirmPassword}
               />
               <span className="reset-password-card-reveal-icon">
-                <button type="button" onClick={() => {}}>
-                  <RevealPass />
+                <button type="button" onClick={revealPassword}>
+                  {inputTypePass ? <HiddenPass /> : <RevealPass />}
                 </button>
               </span>
+            </div>
+            <div className="invalid-feedback d-block">
+              {errors.password?.message}
             </div>
           </div>
 
@@ -50,7 +142,7 @@ const CardResetPassword = () => {
             </button>
             <button
               className="btn btn-outline-danger reset-password-card-button"
-              onClick={() => {}}
+              onClick={handleCancel}
             >
               CANCELAR
             </button>
