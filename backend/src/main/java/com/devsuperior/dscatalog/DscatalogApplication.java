1f774bd88945ad9c143482db23aefc4efd965b07
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-12-22 13:45:39 -0300

    First upload test AWS S3

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.dscatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DscatalogApplication {
+import com.devsuperior.dscatalog.services.S3Service;
 
+@SpringBootApplication
+public class DscatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("C:\\Temp\\foto-perfil.png");
+		
+	}
+
+	
 }
