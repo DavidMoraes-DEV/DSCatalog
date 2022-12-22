6d8bfb980a03fbf52f283c3c95b111d83b0af623
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-12-21 22:34:11 -0300

    Add ProductFilter in CatalogPage

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,7 +1,8 @@
 import { AxiosRequestConfig } from 'axios';
 import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
-import { useEffect, useState } from 'react';
+import ProductFilter, { ProductFilterData } from 'components/ProductFilter';
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/products';
 import { SpringPage } from 'types/vendor/spring';
@@ -10,39 +11,63 @@ import CardLoader from './CardLoader';
 
 import './styles.css';
 
+type ControlComponentsData = {
+  activePage: number;
+  filterData: ProductFilterData;
+};
+
 const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
   const [isLoading, setIsLoading] = useState(false);
 
-  const getProducts = (pageNumber: number) => {
-    const params: AxiosRequestConfig = {
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+      filterData: { name: '', category: null },
+    });
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({
+      activePage: pageNumber,
+      filterData: controlComponentsData.filterData,
+    });
+  };
+
+  const handleSubmitFilter = (data: ProductFilterData) => {
+    setControlComponentsData({ activePage: 0, filterData: data });
+  };
+
+  const getProducts = useCallback(() => {
+    const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: pageNumber,
-        size: 12,
+        page: controlComponentsData.activePage,
+        size: 8,
+        name: controlComponentsData.filterData.name,
+        categoryId: controlComponentsData.filterData.category?.id,
       },
     };
 
     setIsLoading(true);
-    requestBackend(params)
+    requestBackend(config)
       .then((response) => {
         setPage(response.data);
       })
       .finally(() => {
         setIsLoading(false);
       });
-  };
+  }, [controlComponentsData]);
 
   useEffect(() => {
-    getProducts(0);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    getProducts();
+  }, [getProducts]);
 
   return (
     <div className="container my-4 catalog-container">
-      <div className="row catalog-title-container">
+      <div className="catalog-bar-container">
         <h1>Catálogo de produtos</h1>
+        <ProductFilter onSubmitFilter={handleSubmitFilter} />
       </div>
 
       <div className="row catalog-grid-container">
@@ -64,11 +89,12 @@ const Catalog = () => {
         )}
       </div>
 
-      <div className="row">
+      <div>
         <Pagination
+          forcePage={page?.number}
           pageCount={page ? page.totalPages : 0}
-          range={3}
-          onChange={getProducts}
+          range={10}
+          onChange={handlePageChange}
         />
       </div>
     </div>
