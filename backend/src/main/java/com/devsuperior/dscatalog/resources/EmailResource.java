c1830b86acd3c1e82687a44aee7c24a459c5d8ac
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-01-06 15:14:18 -0300

    Add EmailResource and EmailService

--- backend/src/main/java/com/devsuperior/dscatalog/resources/EmailResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,14 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
 	
+	@Autowired
+	private EmailService service;
+	
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
 		
+		service.sendEmail(dto);
 		
 		return ResponseEntity.noContent().build();
 	}
